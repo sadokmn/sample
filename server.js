139aae97f63c761a3ace4890f86b01d5aa263e46
github_sadokmn <gere.sadok209@gmail.com>
2018-06-04 16:47:34 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const hbs = require("hbs");
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials")
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"))
+
+hbs.registerHelper("getCurrentYear", () => {
+    return new Date().getFullYear()
+})
+
+app.get("/", (req, res) => {
+    res.render("index.hbs", {
+        pageTitle: "Home Page",
+        wellomeMesage: "Welcome to my website",
+        
+    });
+});
+
+
+app.get("/about", (req, res) => {
+    res.render("about.hbs", {
+        pageTitle: "About Page",
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get("/bad", (req, res) => {
+    res.send({
+        errorMessage: "error"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("server is up on port 3000");
+});
