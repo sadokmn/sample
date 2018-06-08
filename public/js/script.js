1be0a4b7d00a0eebb0dde4832438d4f5af6dcd02
github_sadokmn <gere.sadok209@gmail.com>
2018-06-08 14:10:13 +0300

    new design update

--- public/js/script.js
+++ public/js/script.js
@@ -6,11 +6,18 @@ var year = d.getFullYear();
 document.querySelector(".footer").innerHTML = '<p>Created by philmon - Copyright &copy; '+year+'</p>'
 
 socket.on("dukan", function (data) {
-    for(var i = 0; i < data.length; i++){
-        var element = document.querySelector(".nayarat")
-        var text = '<div class="col-sm-12 col-md-4 theBody"><div class="cardh-100"><a href=""><img class="card-img-top" src="'+data[i].img+'" alt=""></a><div class="card-body"><h3>'+data[i].sm+'</h3><p>መሸፈኒ: '+data[i].meshefeni+'</p><p>ኣንሶላ: '+data[i].ansola+'</p><p>መተርኣስ: '+data[i].meteras+'</p><h5>'+data[i].waga+'</h5></div><div><small class="kokob">&#9733; &#9733; &#9733; &#9733; &#9734;</small></div></div></div> '        
-        element.insertAdjacentHTML("beforeend", text);
+
+    var aratFun = function (arawti) {
+        var element = document.querySelector(".nayarat");
+        var image = 0;
+        for(var i = 0; i < arawti.length; i++){
+            image++
+            var text = '<div class="col-sm-12 col-md-4 theBody"><div class="cardh-100"><a href="http://localhost:3000/data/arat/arat'+image+'.jpg"><img class="card-img-top" src="http://localhost:3000/data/arat/arat'+image+'.jpg" alt=""></a><div class="card-body"><h3>'+arawti[i].sm+'</h3><h5>መሸፈኒ : '+arawti[i].meshefeni+'</h5><h5>ኣንሶላ : '+arawti[i].ansola+'</h5><h5>መተርኣስ : '+arawti[i].meteras+'</h5><h4><bold>'+arawti[i].waga+'</bold></h4></div><div><small class="kokob">&#9733; &#9733; &#9733; &#9733; &#9734;</small></div></div></div> '        
+            element.insertAdjacentHTML("beforeend", text);
+        };
     };
+    aratFun(data)
+   
 })
 
 {/* <div class="col-sm-12 col-md-4 theBody">
